ccc474a0d66840b340e808db8a8546ea2f0c0e2f
doctype-Melvin <doctype.melvin@gmail.com>
2022-11-05 11:02:54 +0100

    <ES>Jest plugin config

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,7 +1,7 @@
 const Gameboard = (vessel) => {
   const array = [];
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
+  for (let i = 0; i < 10; i += 1) {
+    for (let j = 0; j < 10; j += 1) {
       array.push([`${i},${j}`]);
     }
   }
