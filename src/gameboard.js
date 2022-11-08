58b4eadd34da29c70fca9fd6d65a0c7f40637cf7
doctype-Melvin <doctype.melvin@gmail.com>
2022-11-08 19:33:53 +0100

    <JS>stores ship types in SHIPS array

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,12 +1,19 @@
 import ship from './ship';
 
 const Gameboard = () => {
+  const SHIPS = [
+    { carrier: ship('carrier', 5, 'H') },
+    { battleship: ship('battleship', 4, 'H') },
+    { criuser: ship('cruiser', 3, 'H') },
+    { submarine: ship('submarine', 3, 'H') },
+    { destroyer: ship('destroyer', 2, 'H') },
+  ];
   const board = [];
 
   for (let x = 0; x < 10; x += 1) {
     for (let y = 0; y < 10; y += 1) {
       board.push({
-        psotion: [x, y],
+        position: [x, y],
         vessel: null,
       });
     }
@@ -14,6 +21,8 @@ const Gameboard = () => {
 
   return {
     board,
+    SHIPS,
   };
 };
+
 export default Gameboard;
