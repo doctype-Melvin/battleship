5e060421a2d21dd2101d0f1eb51e9b66d1f1904e
doctype-Melvin <doctype.melvin@gmail.com>
2022-11-17 11:29:48 +0100

    <JS>adds fn to create and render grid

---
+++ src/DOM.js
@@ -0,0 +1,15 @@
+const createGrid = (hand) => {
+  let grid = null;
+  const container = document.getElementById(hand);
+  container.style.display = 'grid';
+  container.style.gridTemplateRows = 'repeat(10, 1fr)';
+  container.style.gridTemplateColumns = 'repeat(10, 1fr)';
+  for (let i = 0; i < 100; i += 1) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    container.append(cell);
+    grid = document.querySelectorAll('.cell');
+  }
+};
+
+module.exports = createGrid;
--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
 import './style.css';
 import Gameboard from './gameboard.js';
 import ship from './ship.js';
+
+const createGrid = require('./DOM');
+
+createGrid('left');
