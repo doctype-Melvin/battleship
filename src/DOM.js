c2941cf12bdfc38ca7cdef5a4dbc40cb3faaba75
doctype-Melvin <doctype.melvin@gmail.com>
2022-11-18 11:58:55 +0100

    <JS>adds ship buttons dynamically <CSS>adds flex and basic styling to menu section

--- src/DOM.js
+++ src/DOM.js
@@ -35,12 +35,22 @@ const DOM = () => {
   const Menu = () => {
     const container = document.querySelector('.menu');
     const score = document.createElement('div');
+    score.classList.add('score');
     const info = document.createElement('div');
+    info.classList.add('info');
     const harbor = document.createElement('div');
+    harbor.classList.add('harbor');
+    const makeShipBtn = (name) => {
+      const btn = document.createElement('button');
+      btn.classList.add('ship');
+      btn.textContent = `${name}`.toUpperCase();
+      return btn;
+    };
+    Gameboard().SHIPS.forEach((ship) => harbor.append(makeShipBtn(ship.name)));
     const rotate = document.createElement('button');
+    rotate.classList.add('rotate');
     score.textContent = 'Score';
     info.textContent = 'This game is under construction';
-    harbor.textContent = 'Carrier, Battleship, Submarine, Cruiser, Destroyer';
     rotate.textContent = 'Rotate Ship';
     container.append(score, info, harbor, rotate);
   };
