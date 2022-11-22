1b2f1ff6df00d985f6a2e6333bc6d0870b4fc594
doctype-Melvin <doctype.melvin@gmail.com>
2022-11-22 13:09:20 +0100

    <JS>starting point of game logic and DOM

--- src/DOM.js
+++ src/DOM.js
@@ -1,94 +1,11 @@
 const Gameboard = require('./gameboard');
 
-const DOM = () => {
-  // Grid factory
-  const Grid = (...grids) => {
-    let gridLeft = null;
-    let gridRight = null;
-    const createGrid = (side) => {
-      for (let i = 0; i < 100; i += 1) {
-        const container = document.getElementById(side);
-        const cell = document.createElement('div');
-        cell.setAttribute('datacoor', JSON.stringify(Gameboard().ocean[i]));
-        if (side === 'left') {
-          cell.classList.add('cellLeft');
-        } else {
-          cell.classList.add('cellRight');
-        }
-        container.append(cell);
-        gridLeft = document.querySelectorAll('.cellLeft');
-        gridRight = document.querySelectorAll('.cellRight');
-      }
-    };
-    grids.forEach((side) => createGrid(side));
-
-    const placeShipLeft = () => gridLeft.forEach((cell) => cell.addEventListener('click', (e) => console.log(e.target)));
-
-    gridRight.forEach((cell) => cell.addEventListener('click', (e) => {
-      console.log(e.target);
-    }));
-
-    const reset = (side) => {
-      const container = document.getElementById(side);
-      while (container.firstChild) {
-        container.removeChild(container.lastChild);
-      }
-    };
-
-    const resetGrid = (...boards) => {
-      boards.forEach((board) => reset(board));
-    };
-
-    return {
-      resetGrid,
-      gridLeft,
-      gridRight,
-      placeShipLeft,
-    };
+const DOM = (() => {
+  // Creates DOM elements and assigns a class to them
+  const elementFactory = (type, cl) => {
+    const element = document.createElement(type);
+    element.classList.add(cl);
+    return element;
   };
-
-  const Menu = () => {
-    const container = document.querySelector('.menu');
-    const score = document.createElement('div');
-    score.classList.add('score');
-    const info = document.createElement('div');
-    info.classList.add('info');
-    const harbor = document.createElement('div');
-    harbor.classList.add('harbor');
-    // Ship Btns
-    const makeShipBtn = (name) => {
-      const btn = document.createElement('button');
-      btn.classList.add('ship');
-      btn.textContent = `${name}`.toUpperCase();
-      return btn;
-    };
-    Gameboard().SHIPS.forEach((ship) => harbor.append(makeShipBtn(ship.name)));
-
-    const rotate = document.createElement('button');
-    rotate.classList.add('rotate');
-    const reset = document.createElement('button');
-    reset.classList.add('reset');
-    score.textContent = 'Score';
-    info.textContent = 'This game is under construction';
-    rotate.textContent = 'Rotate Ship';
-    reset.textContent = 'Reset';
-    container.append(score, info, harbor, rotate, reset);
-
-    const shipBtns = document.querySelectorAll('.ship');
-    shipBtns.forEach((btn) => btn.addEventListener('click', async (e) => {
-      try {
-        const name = await e.target.textContent.toLowerCase();
-        const shipSize = await Gameboard().getShip(name).type.size;
-        console.log(shipSize);
-      } catch (err) {
-        console.log('Error');
-      }
-    }));
-  };
-
-  return {
-    Grid,
-    Menu,
-  };
-};
+})();
 module.exports = DOM;
--- src/game.js
+++ src/game.js
@@ -1,29 +1,18 @@
 const player = require('./player');
 const DOM = require('./DOM');
+const Gameboard = require('./gameboard');
 
 const battleship = () => {
-  let human = player('human');
-  let bot = player('bot');
-  DOM().Menu();
-  DOM().Grid('left', 'right');
+  const human = player('human');
+  const bot = player('bot');
+
   if (bot.board.inGame < 5) {
     bot.setRandomShip();
   }
 
-  const gameReset = () => {
-    human = player('human');
-    bot = player('bot');
-    DOM().Grid().resetGrid('left', 'right');
-    DOM().Grid('left', 'right');
-    return {
-      human,
-      bot,
-    };
-  };
   return {
     human,
     bot,
-    gameReset,
   };
 };
 
