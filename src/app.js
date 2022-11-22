5b4f7e806fbc3e5dffac4a60f7104244fad81979
doctype-Melvin <doctype.melvin@gmail.com>
2022-11-22 13:52:30 +0100

    <ALL>rewrite of project

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import './style.css';
-import Gameboard from './gameboard.js';
-import ship from './ship.js';
-
-const battleship = require('./game');
-
-battleship();
--- src/game.spec.js
+++ src/game.spec.js
@@ -1,11 +1,1 @@
 const battleship = require('./game');
-
-describe.skip('Game loop module', () => {
-  it('Initializes two players and the respective gameboards', () => {
-    expect(battleship().human.board.ocean).toHaveLength(100);
-    expect(battleship().bot.board.inGame).toHaveLength(5);
-  });
-  it('resets the players', () => {
-    expect(battleship().gameReset().human.board.inGame).toHaveLength(0);
-  });
-});
