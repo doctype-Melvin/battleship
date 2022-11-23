bce0d1a9dd9ca587e036185747cd7c86b97fb621
doctype-Melvin <doctype.melvin@gmail.com>
2022-11-23 07:00:22 +0100

    <ALL>moves index.html and style.css to root folder

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       hash: true,
-      template: './src/index.html',
+      template: './index.html',
       filename: 'index.html',
     }),
   ],
